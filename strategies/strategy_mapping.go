2afbeff90c0002fb103cca9c3f2319db234bf5c6
alex.zakarov <talha.semerci394@gmail.com>
2025-08-24 15:42:45 +0300

    rearrange activation function

--- strategies/strategy_mapping.go
+++ strategies/strategy_mapping.go
@@ -8,10 +8,10 @@ import (
 
 func GetStrategies() map[string]domain.IStrategyModel {
 	return map[string]domain.IStrategyModel{
-		"AIStrategyV1": NewAIStrategyV1(1200, 80, 200, 0.001, &ffnnDomain.Config{
-			Inputs:     1213,
+		"AIStrategyV1": NewAIStrategyV1(1400, 80, 50, 0.001, &ffnnDomain.Config{
+			Inputs:     1413,
 			Layout:     []int{100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 3},
-			Activation: ffnnDomain.ActivationReLU,
+			Activation: ffnnDomain.ActivationLeakyReLU,
 			Mode:       ffnnDomain.ModeRegression,
 			Weight:     synapse.NewNormal(1e-20, 1e-20),
 			Bias:       true,
